851fdfa00eb9fd600982d1d4e85827bf4ad93f9d
E1aliu <endritaliu09@gmail.com>
2021-11-21 12:34:31 +0100

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Users from '../../components/pages/Users'
+const Home = () => {
+    return (
+        <div className='home-page'>
+             <Users /> 
+        </div>
+    )
+}
+
+export default Home
